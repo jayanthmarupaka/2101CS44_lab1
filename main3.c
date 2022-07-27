8fd41e8026a79f589799f033748e1dc0755040b8
jayanthmarupaka <95174580+jayanthmarupaka@users.noreply.github.com>
2022-07-27 21:33:21 +0530

    Add files via upload

---
+++ main3.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main() {
+int first ,second;
+char op;
+ printf("Enter an operator (+, -, *, /): ");
+  scanf("%c", &op);
+  printf("Enter two operands: ");
+  scanf("%lf %lf", &first, &second);
+
+  
+
+  return 0;
+}
